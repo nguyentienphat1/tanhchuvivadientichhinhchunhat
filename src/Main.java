20b30eb280a17b2ef054c85b7eaa53cc0d3fa709
Khac Huy <nguyentienphat13@gmail.com>
2023-03-06 18:08:41 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a=2;
+        int b=5;
+        int chuvi,dientich;
+        Scanner scanner= new Scanner(System.in);
+        chuvi = (a+b)*2;
+        System.out.println("kết quả của chu vi là:"+chuvi);
+        dientich = a*b;
+        System.out.println("kết quả diện tích là:"+dientich);
+    }
+}
